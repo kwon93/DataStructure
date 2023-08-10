2c59f428161e8c803a4784bec654b906376c6a36
kwon93 <93umsongaji@gmail.com>
2023-08-10 23:48:15 +0900

    heap add 구현중...

--- interface_form/heap/Heap.java
+++ interface_form/heap/Heap.java
@@ -2,6 +2,12 @@ package interface_form.heap;
 
 import java.util.Comparator;
 
+/** 힙의 성질
+ * 1. 왼쪽 자식 노드 인덱스 = 부모 노드 인덱스 × 2
+ * 2. 오른쪽 자식 노드 인덱스 = 부모 노드 인덱스 × 2 + 1
+ * 3. 부모 노드 인덱스 = 자식 노드 인덱스 / 2
+ */
+
 public class Heap<E> {
 
     private final Comparator<? super E> comparator;
@@ -61,5 +67,38 @@ public class Heap<E> {
 
         this.array = null; //gc처리
         this.array = newArray;
+    }
+
+    public void add(E value){
+        //배열 용적이 꽉 차있을 경우 용적을 두 배로 늘려준다.
+        if (size + 1 == array.length){
+            resize(array.length * 2);
+        }
+
+        siftUp(size + 1, value);
+        size++;
+    }
+
+    private void siftUp(int idx, E target){
+        //comparator가 존재할 경우 comparator를 인자로 넘겨준다.
+        if (comparator != null){
+            siftUpComparator(idx,target,comparator);
+        }else {
+            siftUpComparable(idx, target);
+        }
+    }
+
+    //comparator를 이용한 sift-up
+    @SuppressWarnings("unchecked")
+    private void siftUpComparator(int idx,E target,Comparator<? super E> comp){
+
+        //root노드보다 클 때 까지만 탐색한다.
+
+
+
+    }
+
+    private void siftUpComparable(int idx, E target){
+
     }
 }
